598003333ffe7bde3fe225b2ff1f2d7a178c865a
Antti Haavikko <antti.haavikko@gmail.com>
2025-11-04 15:39:05 +0200

    Use shader for white flasher

--- Scripts/flasher.gd
+++ Scripts/flasher.gd
@@ -2,17 +2,12 @@ class_name Flasher
 extends Node
 
 @export var sprites: Array[Sprite2D]
-
-var colors := {} 
-
-func _ready() -> void:
-	for s in sprites:
-		colors[s.name] = s.modulate
+@export var white: Material
 
 func flash():
 	_colorize(Color.WHITE)
 		
 func _colorize(color: Color):
-	for s in sprites: s.modulate = color
+	for s in sprites: s.material = white
 	await get_tree().create_timer(0.05).timeout
-	for s in sprites: s.modulate = colors[s.name]
+	for s in sprites: s.material = null
