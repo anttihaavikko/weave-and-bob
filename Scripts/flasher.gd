31dcc5a2a7248597dd071ad17f7fa480454fbdd7
Antti Haavikko <antti.haavikko@gmail.com>
2025-11-04 17:00:34 +0200

    Simplify flasher

--- Scripts/flasher.gd
+++ Scripts/flasher.gd
@@ -5,9 +5,6 @@ extends Node
 @export var white: Material
 
 func flash():
-	_colorize(Color.WHITE)
-		
-func _colorize(color: Color):
 	for s in sprites: s.material = white
 	await get_tree().create_timer(0.05).timeout
 	for s in sprites: s.material = null
