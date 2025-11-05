698aea4d7f7fa5f84469cf0ee8f30222a7bb60cb
Antti Haavikko <antti.haavikko@gmail.com>
2025-11-05 20:55:14 +0200

    Add map pickup

--- Scripts/game_state.gd
+++ Scripts/game_state.gd
@@ -1,15 +1,12 @@
 extends Node
 
 var has_magazine := true
-var ids: Array[String]
+var map_upgrades := 0
 
-enum PickupType { None, Magazine }
+var ids: Array[String]
 
 func mark(id: String):
 	ids.push_back(id)
 
-func collect(type: PickupType):
-	if type == PickupType.Magazine: has_magazine = true
-	
 func has(id: String) -> bool:
 	return ids.has(id)
--- Scripts/pickup.gd
+++ Scripts/pickup.gd
@@ -1,14 +1,20 @@
 extends Area2D
 
 @export var id: String
-@export var type: GameState.PickupType
+@export var type: PickupType
+@export var sprite: Sprite2D 
+@export var icons: Array[Texture2D]
+
+enum PickupType { None, Magazine, Map }
 
 func _ready() -> void:
+	sprite.texture = icons[type]
 	if GameState.has(id):
 		queue_free()
 
 func _picked(_body: Node2D):
-	GameState.collect(type)
+	if type == PickupType.Magazine: GameState.has_magazine = true
+	if type == PickupType.Map: GameState.map_upgrades += 1
 	GameState.mark(id)
 	Effects.singleton.add_many([0, 1, 3], global_position)
 	queue_free()
