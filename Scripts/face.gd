4a29488fe83c94ffb18bbb298eec36da730f7f25
Antti Haavikko <antti.haavikko@gmail.com>
2025-11-09 17:31:57 +0200

    Add eye blinks

---
+++ Scripts/face.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@export var left_eye: Node2D
+@export var right_eye: Node2D
+
+var size: float
+var removed: bool
+
+func _ready() -> void:
+	size = left_eye.scale.x
+	blink_both()
+
+func blink_both():
+	await get_tree().create_timer(randf_range(1, 3)).timeout
+	if removed: return
+	blink(left_eye)
+	blink(right_eye)
+	if not removed: blink_both()
+	
+
+func _exit_tree() -> void:
+	removed = true
+
+func blink(eye: Node2D):
+	await get_tree().create_timer(randf_range(-1, 1) * 0.2).timeout
+	if removed: return
+	get_tree().create_tween().tween_property(eye, "scale", Vector2(size, 0), 0.2).set_trans(Tween.TRANS_BOUNCE)
+	await get_tree().create_timer(0.15).timeout
+	if removed: return
+	get_tree().create_tween().tween_property(eye, "scale", Vector2(size, size), 0.25).set_trans(Tween.TRANS_BOUNCE)
