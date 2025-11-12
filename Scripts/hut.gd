605670fbe7ba98d3ded5ed51aa0b0146253fb8c3
Antti Haavikko <antti.haavikko@gmail.com>
2025-11-12 21:12:48 +0200

    Add hut chimney

--- Scripts/hut.gd
+++ Scripts/hut.gd
@@ -3,12 +3,21 @@ extends Node
 @export var inside_parts: Array[Node]
 @export var outside_parts: Array[Node]
 @export var area: Area2D
+@export var attic_area: Area2D
+@export var attic_dark: Node
 
 var change_locked := false
 
 func _ready() -> void:
     area.body_entered.connect(entered)
     area.body_exited.connect(exited)
+    attic_area.body_entered.connect(entered_attic)
+    
+func entered_attic(_node: Node2D):
+    var idx := inside_parts.find(attic_dark)
+    if idx > 0:
+        inside_parts.remove_at(idx)
+    attic_dark.hide()
 
 func entered(_node: Node2D):
     if change_locked:
