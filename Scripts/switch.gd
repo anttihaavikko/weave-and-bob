17dd0bfd54dd4fdfb905098ebe31b7187a2780ea
Antti Haavikko <antti.haavikko@gmail.com>
2025-11-16 12:15:50 +0200

    Make switch & hatch more reusable

--- Scripts/switch.gd
+++ Scripts/switch.gd
@@ -2,6 +2,8 @@ class_name Switch
 extends CharacterBody2D
 
 @export var change := 45.0
+@export var door: Door
+@export var rotating_door: RotatingDoor
 
 var start: float
 var on := false
@@ -9,9 +11,11 @@ var on := false
 signal flipped
 
 func _ready() -> void:
-    start = rotation_degrees
+	start = rotation_degrees
+	if rotating_door:
+		flipped.connect(rotating_door.change)
 
 func flip():
-    on = !on
-    get_tree().create_tween().tween_property(self, "rotation_degrees", start + (change if on else 0.0), 0.3).set_trans(Tween.TRANS_BOUNCE)
-    flipped.emit()
+	on = !on
+	get_tree().create_tween().tween_property(self, "rotation_degrees", start + (change if on else 0.0), 0.3).set_trans(Tween.TRANS_BOUNCE)
+	flipped.emit()
