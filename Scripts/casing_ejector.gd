c0941a244bfeeec04cf61c8d9c0fe33a6de93063
Antti Haavikko <antti.haavikko@gmail.com>
2025-11-20 15:49:59 +0200

    Fix casing ejector errors of changing state

--- Scripts/casing_ejector.gd
+++ Scripts/casing_ejector.gd
@@ -27,11 +27,12 @@ func eject():
 	var c := get_next()
 	get_pool().push_back(c)
 	if c is MovableRigidbody:
-		c.freeze = true
+		# c.set_deferred("freeze", true)
+		# c.freeze = true
 		c.global_position = global_position
 		c.move_to(global_position, global_rotation)
 		c.linear_velocity = Vector2.ZERO
 		c.angular_velocity = 0
-		c.freeze = false
+		# c.freeze = false
 		c.apply_impulse(eject_dir.rotated(global_rotation + randf_range(-0.1, 0.1)) * randf_range(50, 100))
 		c.apply_torque_impulse(randf_range(0, 1))
